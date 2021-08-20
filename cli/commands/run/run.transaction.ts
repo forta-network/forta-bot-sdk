0ebdebb6d904b84503e29f2e7c8a299628a01fd2
Haseeb Rabbani <haseeb.rabbani@gmail.com>
2021-08-20 11:08:48 -0400

    remove getTransaction

--- cli/commands/run/run.transaction.ts
+++ cli/commands/run/run.transaction.ts
@@ -1,27 +1,15 @@
-import Web3 from "web3";
-import { HandleTransaction } from "../../../sdk";
 import { assertExists } from '../../utils';
-import { GetAgentHandlers } from "../../utils/get.agent.handlers";
-import { RunTransactionHandlersOnTransaction } from "../../utils/run.transaction.handlers.on.transaction";
+import { RunHandlersOnTransaction } from "../../utils/run.handlers.on.transaction";
 
 // runs agent transaction handlers against a specified transaction
 export type RunTransaction = (txHash: string) => Promise<void>
 
 export function provideRunTransaction(
-  web3: Web3,
-  getAgentHandlers: GetAgentHandlers,
-  runTransactionHandlersOnTransaction: RunTransactionHandlersOnTransaction
+  runHandlersOnTransaction: RunHandlersOnTransaction
 ): RunTransaction {
-  assertExists(web3, 'web3')
-  assertExists(getAgentHandlers, 'getAgentHandlers')
-  assertExists(runTransactionHandlersOnTransaction, 'runTransactionHandlersOnTransaction')
+  assertExists(runHandlersOnTransaction, 'runHandlersOnTransaction')
 
   return async function runTransaction(txHash: string) {
-    const { transactionHandlers } = await getAgentHandlers()
-    if (!transactionHandlers.length) {
-      throw new Error("no transaction handlers found")
-    }
-
-    await runTransactionHandlersOnTransaction(transactionHandlers, txHash)
+    await runHandlersOnTransaction(txHash)
   }
 }
