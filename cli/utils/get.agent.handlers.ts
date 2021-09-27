f705fb45a98ac66d4acf06593f519f8cf368110d
Haseeb Rabbani <haseeb.rabbani@gmail.com>
2021-09-27 09:50:41 -0400

    getAgentHandlers unit test

--- cli/utils/get.agent.handlers.ts
+++ cli/utils/get.agent.handlers.ts
@@ -1,17 +1,18 @@
-import path from 'path'
 import { HandleBlock, HandleTransaction } from "../../sdk"
-import { assertExists } from "."
+import { assertExists, assertIsNonEmptyString } from "."
 import { GetPythonAgentHandlers } from './get.python.agent.handlers'
 
 // imports agent handlers from project
 export type GetAgentHandlers = () => Promise<{ transactionHandlers: HandleTransaction[], blockHandlers: HandleBlock[] }>
 
 export function provideGetAgentHandlers(
-  handlerPaths: string[],
-  getPythonAgentHandlers: GetPythonAgentHandlers
+  agentPath: string,
+  getPythonAgentHandlers: GetPythonAgentHandlers,
+  dynamicImport: (path: string) => Promise<any>
 ): GetAgentHandlers {
-  assertExists(handlerPaths, 'handlerPaths')
+  assertIsNonEmptyString(agentPath, 'agentPath')
   assertExists(getPythonAgentHandlers, 'getPythonAgentHandlers')
+  assertExists(dynamicImport, 'dynamicImport')
 
   let blockHandlers: HandleBlock[]
   let transactionHandlers: HandleTransaction[]
@@ -25,21 +26,15 @@ export function provideGetAgentHandlers(
     transactionHandlers = []
     blockHandlers = []
     try {
-      for (let handlerPath of handlerPaths) {
-        if (handlerPath.startsWith(`.${path.sep}`)) {
-          handlerPath = handlerPath.replace(`.${path.sep}`, `${process.cwd()}${path.sep}`)
-        }
-
-        if (handlerPath.endsWith(".py")) {
-          const { handleTransaction, handleBlock} = await getPythonAgentHandlers(handlerPath)
-          if (handleTransaction) transactionHandlers.push(handleTransaction)
-          if (handleBlock) blockHandlers.push(handleBlock)
-        } else {
-          const handlers = await import(handlerPath)
-          const { handleTransaction, handleBlock } = handlers.default
-          if (handleTransaction) transactionHandlers.push(handleTransaction)
-          if (handleBlock) blockHandlers.push(handleBlock)
-        }
+      if (agentPath.endsWith(".py")) {
+        const { handleTransaction, handleBlock} = await getPythonAgentHandlers(agentPath)
+        if (handleTransaction) transactionHandlers.push(handleTransaction)
+        if (handleBlock) blockHandlers.push(handleBlock)
+      } else {
+        const handlers = await dynamicImport(agentPath)
+        const { handleTransaction, handleBlock } = handlers.default
+        if (handleTransaction) transactionHandlers.push(handleTransaction)
+        if (handleBlock) blockHandlers.push(handleBlock)
       }
     } catch (e) {
       throw new Error(`issue getting agent handlers: ${e.message}`)
