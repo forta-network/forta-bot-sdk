cf524d61b70c42a26467b966deae81efde7ca944
Mtchuikov <71787586+Mtchuikov@users.noreply.github.com>
2021-10-09 16:55:36 +0300

    Update transaction.py

--- python-sdk/src/forta_agent/transaction.py
+++ python-sdk/src/forta_agent/transaction.py
@@ -1,17 +1,27 @@
+from dataclasses import dataclass
 from .utils import hex_to_int
 
-
+@dataclass
 class Transaction:
-    def __init__(self, dict):
-        self.hash = dict.get('hash')
-        self.from_ = dict.get('from')
-        self.to = dict.get('to')
-        self.nonce = dict.get('nonce')
-        self.gas = hex_to_int(dict.get('gas'))
-        self.gas_price = hex_to_int(
-            dict.get('gasPrice', dict.get('gas_price')))
-        self.value = hex_to_int(dict.get('value'))
-        self.data = dict.get('data')
-        self.r = dict.get('r')
-        self.s = dict.get('s')
-        self.v = dict.get('v')
+
+    transaction_data: dict
+
+    def __post_init__(self):
+        self.data = self.transaction_data['data']
+
+        self.gas = hex_to_int(self.transaction_data['gas'])
+        self.gas_price = hex_to_int(self.transaction_data.get('gasPrice', self.transaction_data['gas_price']))
+
+        self.hash = self.transaction_data['hash']
+
+        self.nonce = self.transaction_data['nonce']
+
+        self.r = self.transaction_data['r']
+
+        self.s = self.transaction_data['s']
+        self.send_from = self.transaction_data['from']
+        self.send_to = self.transaction_data['to']
+       
+        self.v = self.transaction_data['v']
+        self.value = hex_to_int(self.transaction_data['value'])
+        
