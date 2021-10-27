b536d6323f440b8b62393135d26b45b8f8004511
Haseeb Rabbani <haseeb.rabbani@gmail.com>
2021-10-27 16:40:49 -0400

    limit starter project alerts

--- starter-project/ts/src/agent.ts
+++ starter-project/ts/src/agent.ts
@@ -9,8 +9,13 @@ import {
   FindingType 
 } from 'forta-agent'
 
+let findingsCount = 0
+
 const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
   const findings: Finding[] = []
+
+  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
+  if (findingsCount >= 5) return findings;
 
   // create finding if gas used is higher than threshold
   const gasUsed = new BigNumber(txEvent.gasUsed)
@@ -22,6 +27,7 @@ const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) =
       severity: FindingSeverity.Medium,
       type: FindingType.Suspicious
     }))
+    findingsCount++
   }
 
   return findings
