11ee3efad946ab05c97b83fdc6c08b66b03b7c5e
Haseeb Rabbani <haseeb.rabbani@gmail.com>
2021-12-16 18:16:42 +0400

    actually export keccak256

--- python-sdk/src/forta_agent/__init__.py
+++ python-sdk/src/forta_agent/__init__.py
@@ -7,7 +7,7 @@ from .receipt import Receipt, Log
 from .trace import Trace, TraceAction, TraceResult
 from .event_type import EventType
 from .network import Network
-from .utils import get_json_rpc_url, create_block_event, create_transaction_event, get_web3_provider
+from .utils import get_json_rpc_url, create_block_event, create_transaction_event, get_web3_provider, keccak256
 from web3 import Web3
 
 web3Provider = Web3(Web3.HTTPProvider(get_json_rpc_url()))
--- sdk/index.ts
+++ sdk/index.ts
@@ -3,7 +3,7 @@ import { Finding, FindingSeverity, FindingType } from "./finding"
 import { BlockEvent } from "./block.event"
 import { Block } from "./block"
 import { TransactionEvent, TxEventBlock, LogDescription } from "./transaction.event"
-import { createBlockEvent, createTransactionEvent, getJsonRpcUrl, getEthersProvider, getEthersBatchProvider } from "./utils"
+import { createBlockEvent, createTransactionEvent, getJsonRpcUrl, getEthersProvider, getEthersBatchProvider, keccak256 } from "./utils"
 import { Log, Receipt } from "./receipt"
 import { Trace, TraceAction, TraceResult } from "./trace"
 import { Transaction } from "./transaction"
@@ -68,4 +68,5 @@ export {
   getEthersProvider,
   getEthersBatchProvider,
   ethers,
+  keccak256
  }
