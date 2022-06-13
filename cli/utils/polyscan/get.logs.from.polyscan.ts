d5037f594c02b737490ef8691e90908e5c6aab8c
Robert Leonard <robertleonard@Roberts-MacBook-Pro.local>
2022-06-13 08:55:44 -0400

    Adding retry logic for polyscan query

--- cli/utils/polyscan/get.logs.from.polyscan.ts
+++ cli/utils/polyscan/get.logs.from.polyscan.ts
@@ -1,11 +1,12 @@
 import { AxiosStatic } from "axios";
 import { assertExists } from "..";
 import { StateChangeContractEvent } from "../../contracts/agent.registry";
+import retry from "async-retry";
 
 
 export interface EventFilter{
     type: StateChangeContractEvent
-    topic_0: string
+    topicHash: string
 }
 
 export interface PolyscanLog{
@@ -35,28 +36,41 @@ export default function provideGetLogsFromPolyscan(
                 topic3: agentId,
                 topic0_3_opr: "and"
             }
+        let logData: PolyscanLog[] = []
 
+        const apiKey = getApiKey()
 
-        const response = await axios.get(polyscanApiUrl, {
-            headers: {
-              "accept": "application/json",
-            },
-            params: {
-              apikey: "UYNC8TFCFJ9HJVMMY9Z6X4DVW2W5K1NII2",
-              module: "logs",
-              action: "getLogs",
-              address,
-              topic0: eventFilter.topic_0,
-              ...agentIdFilterParams
-            }
-          });
+        await retry(async ()=> {
+            const response = await axios.get(polyscanApiUrl, {
+                headers: {
+                  "accept": "application/json",
+                },
+                params: {
+                  apikey: apiKey,
+                  module: "logs",
+                  action: "getLogs",
+                  address,
+                  topic0: eventFilter.topicHash,
+                  ...agentIdFilterParams
+                }
+            });
+    
+              const {data} = response;
+    
+              const hasData = data && data.result && data.result.length > 0;
+    
+              if(typeof data.result === 'string') {
+                  throw Error(`Failed to call polyscan api: ${data.result}`)
+              }
+    
+              logData = hasData ? data.result : []
+        }, { retries: 3})
 
-          const {data} = response
-
-          if(typeof data.result === 'string') {
-              throw Error(`Failed to call polyscan api: ${data.result}`)
-          }
-          const hasData = data && data.result && data.result.length > 0;
-          return hasData ? data.result : []
+        return logData
     }
+}
+
+// Randomly selects an api key to use for polyscan. Each api key allows us 5 req/sec
+const getApiKey = (): string => {
+    return (Math.floor(Math.random() * 10) % 2) === 0 ? "UYNC8TFCFJ9HJVMMY9Z6X4DVW2W5K1NII2" : "GVEAVC3WQYH9JH26MHD7THVQRQ5U5SP9R8"
 }
