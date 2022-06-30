e40423eb56099aa87c220a7849740e77c655adb2
Robert Leonard <robertleonard@Roberts-MacBook-Pro-2.local>
2022-06-30 13:05:46 -0400

    Updating alert interface

--- sdk/alert.ts
+++ sdk/alert.ts
@@ -3,8 +3,8 @@ export interface Alert {
     alertId: string,
     contracts: {
         address: string,
-        name: string,
-        projectId: string
+        name?: string,
+        projectId?: string
     }[],
     createdAt: string,
     description: string,
@@ -14,6 +14,40 @@ export interface Alert {
     scanNodeCount: number,
     severity: string,
     source: {
-        transactionHash: string
+        transactionHash: string,
+        block?: {
+            timestamp: string,
+            chainId: number,
+            hash: string,
+            number: number // block number
+        }
+        bot: {
+            id: string,
+            reference?: string,
+            image?: string,
+        }
     }
+    metadata?: any,
+    projects?: {
+        id: string,
+        name?: string,
+        contacts?: {
+            securityEmailAddress: string,
+            generalEmailAddress: string
+        },
+        website?: string,
+        token?: {
+            symbol: string,
+            name: string,
+            decimals: number,
+            chainId: number,
+            address: string
+        },
+        social?:{
+            twitter: string,
+            github: string,
+            everest: string,
+            coingecko: string
+        }
+    } []
 }
