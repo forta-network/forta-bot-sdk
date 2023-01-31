9ce31080ccca85f11ec4ed4de2594290b154c310
Caner Çıdam <canercidam01@gmail.com>
2023-01-31 18:17:50 -0300

    fix utility method

--- cli/utils/register/injected.ts
+++ cli/utils/register/injected.ts
@@ -1,24 +1,24 @@
 import configureContainer from "../../di.container";
-import { ethers, providers, Signer } from "ethers"
-import { assertExists } from "..";
-import { asFunction, asValue } from "awilix"
+import { Signer } from "ethers"
+import { asValue } from "awilix"
 import { GetFromIpfs } from "../ipfs/get.from.ipfs";
 import { PushToRegistry } from "../../commands/publish/push.to.registry";
 
-export async function registerWithCustomEthersProvider(
-  provider: providers.JsonRpcProvider,
+export async function registerWithInjectedSigner(
   signer: Signer,
   manifestReference: string
 ) {
   const diContainer = configureContainer();
-  diContainer.register('ethersAgentRegistryProvider', asValue(provider))
+  diContainer.register('ethersAgentRegistryProvider', asValue(signer.provider))
   diContainer.register('appendToFile', asValue(() => {})) // no-op
   
   const getFromIpfs = diContainer.resolve<GetFromIpfs>('getFromIpfs')
   const manifest = await getFromIpfs(manifestReference)
 
-  diContainer.register('agentId', asValue(manifest.manifest.agentId))
+  diContainer.register('agentId', asValue(manifest.manifest.agentIdHash))
   diContainer.register('chainIds', asValue(manifest.manifest.chainIds))
+
+  console.log(diContainer.resolve('agentId'))
 
   const pushToRegistry = diContainer.resolve<PushToRegistry>('pushToRegistry')
   await pushToRegistry(manifestReference, signer)
